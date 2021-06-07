49d390a16da64cd251b74b99595ab0e3b7e7adca
Balanjaneyasharma <69688211+Balanjaneyasharma@users.noreply.github.com>
2021-06-07 13:08:48 +0530

    Create ArrayReverse.cpp

---
+++ ArrayReverse.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<array>
+using namespace std;
+void ArrayReverse(int l,int r, int a[])
+{
+   if(l>r)
+   {
+      return;
+   }
+   swap(a[l],a[r]);
+   return ArrayReverse(l+1,r-1,a);
+}
+int main()
+{
+   int n;
+   cin>>n;
+   int a[n];
+   for(int i=0;i<n;i++)
+   {
+      cin>>a[i];
+   }
+   ArrayReverse(0,n-1,a);
+   for(int i=0;i<n;i++)
+   {
+      cout<<a[i]<<' ';
+   }
+   return 0;
+}
